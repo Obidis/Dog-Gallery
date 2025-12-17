94c32f12dd9827c9f32bc74c8fbc6d93e9b6ce49
Juan José Santos Fernandez <jjsantosfernandez02@gmail.com>
2025-12-17 13:15:58 +0100

    actualizando ruta a img

---
+++ assets/favorites-BKMEO-Ug.js
@@ -0,0 +1,1 @@
+import{G as o}from"./main-4b4lHhiY.js";let e=new o;e.loader();e.mostrarFavoritos();e.eliminarFavorito();document.getElementById("delete-favorites").onclick=e.clearLocalStorage.bind(e);
