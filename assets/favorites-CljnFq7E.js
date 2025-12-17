5610c07d9fd57a8bf628b521f4eb90c606802479
Juan José Santos Fernandez <jjsantosfernandez02@gmail.com>
2025-12-17 13:02:47 +0100

    actualizando img

---
+++ assets/favorites-CljnFq7E.js
@@ -0,0 +1,1 @@
+import{G as o}from"./main-BV8HUqZH.js";let e=new o;e.loader();e.mostrarFavoritos();e.eliminarFavorito();document.getElementById("delete-favorites").onclick=e.clearLocalStorage.bind(e);
---
+++ assets/main-bCbIx2y9.js
@@ -0,0 +1,1 @@
+import{G as t}from"./main-BV8HUqZH.js";let e=new t;e.mostrarImagenes();e.loader();e.getDogsBreeds();document.getElementById("btn").addEventListener("click",e.mostrarImagenes.bind(e));document.getElementById("more-button").addEventListener("click",e.masImagenes.bind(e));e.saveToLocalStorage.bind(e)();
