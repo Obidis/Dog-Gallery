81c7a3e0c89103b67a2b6be78c117ecaca60742e
Juan José Santos Fernandez <jjsantosfernandez02@gmail.com>
2025-12-17 11:44:37 +0100

    Add files via upload

---
+++ assets/favorites-DQx-fFsT.js
@@ -0,0 +1,1 @@
+import{G as o}from"./main-B0SAkeQB.js";let e=new o;e.loader();e.mostrarFavoritos();e.eliminarFavorito();document.getElementById("delete-favorites").onclick=e.clearLocalStorage.bind(e);
